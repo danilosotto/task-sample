1397eb4c3b856f658171c75dbb8f290154fa0548
Danilo Sotto De Lima <60400027+danilosotto@users.noreply.github.com>
2023-09-30 21:13:31 -0300

    aadd

---
+++ src/main/java/org/api/Main.java
@@ -0,0 +1,19 @@
+package org.api;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+
+@SpringBootApplication
+@EnableTask
+public class Main implements CommandLineRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("testeee");
+    }
+}
